f8ea6b544b4c9b5f1d5664a41bf73af3105978b2
andrea-chirillano <andreatchirillano@hotmail.com>
2025-01-27 23:28:12 -0300

    Add: hooks and css styles

--- functions.php
+++ functions.php
@@ -1,13 +1,25 @@
 <?php
 function dhd_enqueue_styles()
 {
-    // Cargar los estilos del tema padre
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
-    // Cargar los estilos del tema hijo
     wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 
-    // Cargar otros estilos adicionales
     wp_enqueue_style('skeleton-style', get_stylesheet_directory_uri() . '/skeleton.css', array('child-style'));
+
+    wp_enqueue_script('navbar-scroll', get_stylesheet_directory_uri() . '/js/navbar-scroll.js', array('jquery'), null, true);
 }
 add_action('wp_enqueue_scripts', 'dhd_enqueue_styles');
+
+function my_custom_home_top_sellers() {
+    if (is_front_page()) {
+        echo '<div class="home-top-sellers" style="padding: 20px; background-color: #f7f7f7; margin-top: 20px; border: 1px solid #ddd; border-radius: 10px;">';
+        echo '<h2 style="text-align: center; margin-bottom: 20px;">🔥 Best selling products</h2>';
+        
+        echo do_shortcode('[best_selling_products limit="4" columns="4"]');
+        
+        echo '</div>';
+    }
+}
+add_action('woocommerce_after_main_content', 'my_custom_home_top_sellers');
+?>
